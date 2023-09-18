6250fa9ffaa25892250b053c456f15e8d705ce90
Vladimir Cvetic <vladimir@ferdinand.rs>
2023-09-18 08:49:39 +0200

    Delete backend directory

--- backend/src/extensions/upload/strapi-server.js
+++
@@ -1,5 +0,0 @@
-module.exports = (plugin) => {
-    const imageOptimizerService = require("strapi-plugin-image-optimizer/dist/server/services/image-optimizer-service");
-    plugin.services["image-manipulation"] = imageOptimizerService;
-    return plugin;
-};
